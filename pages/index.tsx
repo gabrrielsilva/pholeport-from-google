f270ad5d069a2b1cdf32cfdca6cfe8d2339d7ce5
gabrrielsilva <gabrielsilvaev@gmail.com>
2022-12-11 22:17:58 -0300

    created: map.setStreetView

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
 import { useEffect, useRef } from 'react';
 
+const LOCATION = { lat: -23.568333, lng: -46.623366 };
+
 export default function Main() {
-  const mapsContainer = useRef<HTMLElement>(null);
+  const panoramaContainer = useRef<HTMLElement>(null);
+  const mapContainer = useRef<HTMLElement>(null);
   const coords = useRef<HTMLParagraphElement>(null);
   const address = useRef<HTMLParagraphElement>(null);
 
-  function initMap() {
+  function initialize() {
     const panorama = new google.maps.StreetViewPanorama(
-      mapsContainer.current as HTMLElement,
+      panoramaContainer.current as HTMLElement,
       {
-        position: { lat: -23.562755, lng: -47.457893 },
+        position: LOCATION,
         pov: { heading: 270, pitch: 0 },
-        visible: true
+        visible: true,
+        disableDefaultUI: true,
+        zoom: 0
       }
     )
   
@@ -23,22 +28,39 @@ export default function Main() {
       const coords = panorama.getLinks() as google.maps.StreetViewLink[];
       (address.current as HTMLElement).innerText  = coords[0]?.description as string;
     })
+
+    const map = new google.maps.Map(
+      mapContainer.current as HTMLElement,
+      {
+        center: LOCATION,
+        zoom: 18,
+      }
+    );
+
+    map.setStreetView(panorama);
   }
 
   useEffect(() => {
-    if (google) initMap();
+    if (google) initialize();
   }, [])
 
   return (
     <>
-      <main className='flex h-screen w-screen'>
-        <div className='grid grid-cols-2 w-full h-full bg-[#121212]'>
-          <section ref={mapsContainer} className='relative overflow-hidden'>
-            <details open className='absolute bottom-0 left-0 z-10 bg-white h-max w-max p-2 rounded-tr-md'>
-              <p ref={coords}></p>
-              <p ref={address}></p>
-            </details>
-          </section>
+      <main className='flex items-center justify-center w-screen h-screen bg-[#121212]'>
+        <div className='w-full max-w-6xl h-4/5'>
+          <div className='grid w-full h-full grid-cols-2'>
+            <section ref={panoramaContainer} className='relative overflow-hidden'>
+              <details  open className='absolute bottom-0 left-0 z-10 p-2 pb-5 text-xl bg-white h-max w-max rounded-tr-md'>
+                <summary>Coords and Address</summary>
+                <p ref={coords}></p>
+                <p ref={address}></p>
+              </details>
+            </section>
+            <div className='h-full'>
+              <section ref={mapContainer} className='w-full h-1/2'></section>
+              <section className='w-full bg-indigo-800 h-1/2'></section>
+            </div>
+          </div>
         </div>
       </main>
     </>
